9d66731170eee266a576d697b636504106f67996
blackpet <blackpet@gmail.com>
2022-01-06 12:55:12 +0900

    push notification

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { onMount, tick } from 'svelte';
-	import { goto } from '$app/navigation';
+	import {onMount, tick} from 'svelte';
+	import {goto} from '$app/navigation';
 	import {
 		appInfo,
 		clearCache,
@@ -10,16 +10,17 @@
 		initAppBridge
 	} from '../libs/nbridge/app-bridge';
 	import UAParser from 'ua-parser-js';
-	import { initLinkBridge, openBrowser } from '../libs/nbridge/link-bridge';
+	import {initLinkBridge, openBrowser} from '../libs/nbridge/link-bridge';
 	import {
 		getPreference,
 		initPreferenceBridge,
 		removePreference,
 		setPreference
 	} from '../libs/nbridge/preference-bridge';
+	import axios from 'axios';
 
 	const uap = new UAParser();
-	const ua = { ua: uap.getUA(), os: uap.getOS(), browser: uap.getBrowser() };
+	const ua = {ua: uap.getUA(), os: uap.getOS(), browser: uap.getBrowser()};
 
 	let nbridge;
 	let logs = [];
@@ -57,7 +58,10 @@
 			console.log('nbridge', nbridge);
 
 			nbridge.setDebugMode(true);
-			nbridge.setLogger({ log });
+			nbridge.setLogger({log});
+
+			version = (await axios.get('/api/version')).data;
+			log('version loaded', version);
 		} catch (err) {
 			log('error!!!', err);
 		}
@@ -93,11 +97,27 @@
 		preferenceAction = 'GET';
 		preferenceKey = 'notification_token';
 		preferenceValue = await getPreference(preferenceKey, 'NO_TOKEN_VALUE');
-		log('callGetToken', preferenceKey, preferenceValue);
+
+		if (preferenceValue) {
+			token = preferenceValue;
+			log('callGetToken', preferenceKey, preferenceValue);
+			log('store token value');
+		}
 	}
 
 	function reload() {
 		location.reload();
+	}
+
+	async function sendPush() {
+		// DEBUG
+		//
+		token ??=
+						'fP-GTzM0P0rYpHAi95fEer:APA91bHKJ3wTRgyHxfvIRKRh1M37M2B7HjDDN18FvKh7NbYbHg11CVo3WNma9mWIE579W6AmMEhXF2mXyHI6lZ2nrtBvebpi9RfTw8PIt9YX4sOdd7Z835UJJq0v7lAth8eao0zW-oTT';
+
+		const data = {...pushData, token};
+		const res = await axios.post('/api/push', data);
+		log(res.data);
 	}
 </script>
 
@@ -111,36 +131,36 @@
 			App DevTools
 			<button class="absolute left-0 text-blue-600 bg-blue-100 rounded p-1" on:click={reload}>
 				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-6 w-6"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
 				>
 					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
 					/>
 				</svg>
 			</button>
 			<button
-				class="absolute right-0 text-blue-600 bg-blue-100 rounded p-1"
-				on:click={() => goto('urls')}
+							class="absolute right-0 text-blue-600 bg-blue-100 rounded p-1"
+							on:click={() => goto('urls')}
 			>
 				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-6 w-6"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
 				>
 					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
 					/>
 				</svg>
 			</button>
@@ -158,16 +178,16 @@
 		<section>
 			<h2>Link Bridge</h2>
 			<div class="pb-2">
-				<input type="text" bind:value={url} placeholder="URL..." />
+				<input type="text" bind:value={url} placeholder="URL..."/>
 			</div>
 			<button class="btn" on:click={() => callNbridge(openBrowser, url)} disabled={!url}
-				>Open Browser
+			>Open Browser
 			</button>
 			<button
-				class="btn"
-				on:click={() => log("[Link External] feature didn't implemented yet!")}
-				disabled={!url}
-				>Link External
+							class="btn"
+							on:click={() => log("[Link External] feature didn't implemented yet!")}
+							disabled={!url}
+			>Link External
 			</button>
 			<p>Open Browser | Link External page</p>
 		</section>
@@ -175,27 +195,51 @@
 		<section>
 			<h2>Preference Bridge</h2>
 			<div class="pb-2 grid grid-cols-1 gap-2 lg:grid-cols-2">
-				<input type="text" bind:value={preferenceKey} placeholder="Key" />
-				<input type="text" bind:value={preferenceValue} placeholder="Value" />
+				<input type="text" bind:value={preferenceKey} placeholder="Key"/>
+				<input type="text" bind:value={preferenceValue} placeholder="Value"/>
 			</div>
 			<button
-				class="btn"
-				on:click={() => callGetPreference('DEFAULT_VALUE')}
-				disabled={!preferenceKey}
-				>Get Preference
+							class="btn"
+							on:click={() => callGetPreference('DEFAULT_VALUE')}
+							disabled={!preferenceKey}
+			>Get Preference
 			</button>
 			<button
-				class="btn"
-				on:click={() => callSetPreference()}
-				disabled={!preferenceKey || !preferenceValue}
-				>Set Preference
+							class="btn"
+							on:click={() => callSetPreference()}
+							disabled={!preferenceKey || !preferenceValue}
+			>Set Preference
 			</button>
 			<button class="btn" on:click={() => callRemovePreference()} disabled={!preferenceKey}
-				>Remove Preference
+			>Remove Preference
 			</button>
 			<button class="btn" on:click={() => callGetToken()}>Get Token</button>
 			<p class="break-all">[{preferenceAction}] {preferenceKey}={preferenceValue}</p>
 		</section>
+
+		{#if token}
+			<section class="bg-green-400">
+				<h2>Push Test</h2>
+				<div class="space-y-1 sm:space-y-0 sm:gap-2 sm:grid sm:grid-cols-3">
+					<input type="text" bind:value={pushData.title} placeholder="Title..."/>
+					<input type="text" bind:value={pushData.body} placeholder="Body..."/>
+					<input type="text" bind:value={pushData.url} placeholder="Target URL..."/>
+				</div>
+				<div class="flex justify-between mt-4">
+					<div>
+						<a href on:click|preventDefault={() => (pushData.url = 'http://naver.com')}>네이버</a> |
+						<a href on:click|preventDefault={() => (pushData.url = 'https://icodi.vercel.app/')}
+						>DevTool</a
+						>
+						|
+						<a href on:click|preventDefault={() => (pushData.url = '')}>직접입력</a>
+					</div>
+					<div>
+						<button class="btn" on:click={sendPush}>Send</button>
+					</div>
+				</div>
+			</section>
+		{/if}
 
 		<section>
 			<h2>User Agent</h2>
@@ -224,8 +268,8 @@
 </div>
 
 <code
-	class="block w-screen h-[50vh] bg-gray-800 overflow-hidden overflow-y-scroll p-2 text-gray-50 text-xs md:text-base"
-	bind:this={code}
+				class="block w-screen h-[50vh] bg-gray-800 overflow-hidden overflow-y-scroll p-2 text-gray-50 text-xs md:text-base"
+				bind:this={code}
 >
 	{#each logs as log, i (i)}
 		<code class="block break-all">#{i}: {log}</code>
